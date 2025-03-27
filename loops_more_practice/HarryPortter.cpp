f46db46b8a13b6fccb30f38c0d52fdc01c2f2cd2
Brandon_Kevin <brandonykevin07@gmail.com>
2025-03-26 22:50:34 -0400

    Completamos harry potter

--- loops_more_practice/HarryPortter.cpp
+++ loops_more_practice/HarryPortter.cpp
@@ -1,47 +1,123 @@
-#include <iostream>
-using namespace std;
-int main()
-{
+#include<bits/stdc++.h>
 
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vi> vii;
+typedef vector<bool> vb;
+
+const int INF = 1e9;
+const long long int LLINF = 4e18;
+const double EPS = 1e-9; // very small number
+
+void solve(){
     int answare = 0;
-    int gryffindor, hufflepuff, ravenclaw, slytherin;
-    cout << "Bievenido, el quiz del sombre magico comienza !! " << endl;
-    cout << "P1) Cuando esté muerto, quiero que la gente me recuerde como: ";
+    int gryffindor = 0, hufflepuff = 0, ravenclaw = 0, slytherin = 0;
+
+    cout << "Bienvenido, el quiz del sombrero magico comienza !! " << endl;
+
+    // P1
+    cout << "P1) Cuando esté muerto, quiero que la gente me recuerde como: " << endl;
     cout << "1) El Bien" << endl;
     cout << "2) El Grande" << endl;
     cout << "3) El Sabio" << endl;
     cout << "4) El Audaz" << endl;
-    cin>> answare;
-    if (answare == 1){
+    cin >> answare;
+
+    if (answare == 1) {
         hufflepuff++;
-    }else if(answare == 2){
+    } else if (answare == 2) {
         slytherin++;
-    }else if(answare == 3){
+    } else if (answare == 3) {
         ravenclaw++;
-    }else if(answare == 4){
+    } else if (answare == 4) {
         gryffindor++;
-    }else{
-        cout<<"Entrada incorrecta";
+    } else {
+        cout << "Entrada incorrecta" << endl;
+        return; // Exit if input is invalid
     }
-    string house;
-    int maximo = max(hufflepuff, slytherin);
-    maximo = max(ravenclaw, gryffindor);
-    if (answare == 1)
-    {
-        cout<<"hufflepuff!!!!";
+
+    // P2
+    cout << "P2) Si tuviera que elegir un compañero, preferiría alguien que sea: " << endl;
+    cout << "1) Fiel y leal" << endl;
+    cout << "2) Ambicioso y astuto" << endl;
+    cout << "3) Inteligente y curioso" << endl;
+    cout << "4) Valiente y decidido" << endl;
+    cin >> answare;
+
+    if (answare == 1) {
+        hufflepuff++;
+    } else if (answare == 2) {
+        slytherin++;
+    } else if (answare == 3) {
+        ravenclaw++;
+    } else if (answare == 4) {
+        gryffindor++;
+    } else {
+        cout << "Entrada incorrecta" << endl;
+        return;
     }
-    else if (answare == 2)
-    {
-        cout<<"slytherin!!!!";
+
+    // P3
+    cout << "P3) ¿Qué habilidad prefieres tener? " << endl;
+    cout << "1) Tratar con criaturas mágicas" << endl;
+    cout << "2) Manipular a los demás" << endl;
+    cout << "3) Resolver acertijos complejos" << endl;
+    cout << "4) Defender a los débiles" << endl;
+    cin >> answare;
+
+    if (answare == 1) {
+        hufflepuff++;
+    } else if (answare == 2) {
+        slytherin++;
+    } else if (answare == 3) {
+        ravenclaw++;
+    } else if (answare == 4) {
+        gryffindor++;
+    } else {
+        cout << "Entrada incorrecta" << endl;
+        return;
     }
-    else if (answare == 3)
-    {
-        cout<<"ravenclaw!!!!";
+
+    // P4
+    cout << "P4) ¿Cómo prefieres resolver un conflicto? " << endl;
+    cout << "1) Negociando pacíficamente" << endl;
+    cout << "2) Usando astucia y estrategia" << endl;
+    cout << "3) Con lógica e intelecto" << endl;
+    cout << "4) Enfrentándolo con valentía" << endl;
+    cin >> answare;
+
+    if (answare == 1) {
+        hufflepuff++;
+    } else if (answare == 2) {
+        slytherin++;
+    } else if (answare == 3) {
+        ravenclaw++;
+    } else if (answare == 4) {
+        gryffindor++;
+    } else {
+        cout << "Entrada incorrecta" << endl;
+        return;
     }
-    else if (answare == 4)
-    {
-        cout<<"gryffindor!!!!";
+
+    if (gryffindor > hufflepuff && gryffindor > ravenclaw && gryffindor > slytherin) {
+        cout << "¡Tu casa es Gryffindor!" << endl;
+    } else if (hufflepuff > gryffindor && hufflepuff > ravenclaw && hufflepuff > slytherin) {
+        cout << "¡Tu casa es Hufflepuff!" << endl;
+    } else if (ravenclaw > gryffindor && ravenclaw > hufflepuff && ravenclaw > slytherin) {
+        cout << "¡Tu casa es Ravenclaw!" << endl;
+    } else if (slytherin > gryffindor && slytherin > hufflepuff && slytherin > ravenclaw) {
+        cout << "¡Tu casa es Slytherin!" << endl;
+    } else {
+        cout << "Hubo un error, no se pudo determinar la casa." << endl;
     }
-//completar por que esta incompleto
-        return 0;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
 }
