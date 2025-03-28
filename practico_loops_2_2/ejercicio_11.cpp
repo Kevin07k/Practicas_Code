a553fd25d40ca70782421cb6be067902ae2d77bd
Brandon Kevin <ibmegta5@gmail.com>
2025-03-28 10:04:25 -0400

    Agregamos el ejericicioÂ 11

--- practico_loops_2_2/ejercicio_11.cpp
+++ practico_loops_2_2/ejercicio_11.cpp
@@ -6,13 +6,23 @@ typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<vi> vii;
 typedef vector<bool> vb;
+typedef vector<double> vd;
 
 const int INF = 1e9;
 const long long int LLINF = 4e18;
 const double EPS = 1e-9; // very small number
 
 void solve(){
-
+    int n;
+    cin>>n;
+    vd secuence(n, -1);
+    secuence[0] = 1;
+    for (int i = 0; i<n; i++) {
+        secuence[i +1] = ((secuence[i]) / (i + 1));
+    }
+    for (int i = 0; i <= n; ++i) {
+        cout<<"U"<<i<<" = "<<secuence[i]<<endl;
+    }
 }
 
 int main(){
