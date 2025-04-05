aafe2ac787bae77791b124521e755c80473407b3
Brandon Kevin <ibmegta5@gmail.com>
2025-04-05 17:02:22 -0400

    Agregamos exercise 2 del examen Practico

--- Examenes_Pasados/Exercise_2_par_2.cpp
+++ Examenes_Pasados/Exercise_2_par_2.cpp
@@ -11,11 +11,31 @@ const int INF = 1e9;
 const long long int LLINF = 4e18;
 const double EPS = 1e-9; // very small number
 
-void solve(){
-
+void solve() {
+    int number = NULL, iterator = 1;
+    long int suma_positive = 0, suma_negative = 0;
+    cout<<"Introducir los numeros para los calculos (El 0 para salir) \n";
+    while (true) {
+        cout<<"Numero "<< iterator<<":";
+        cin >> number;
+        if (number == 0) break;
+        if (number > 0) suma_positive += number;
+        else suma_negative -= number;
+        iterator++;
+    }
+    cout<<"==================================\n";
+    cout<<"Numero ingresados " <<iterator;
+    cout<<"\n";
+    cout<<"Suma Total: "<<suma_positive + suma_negative;
+    cout<<"\n";
+    cout<<"Promedio aritmetico: "<<(suma_positive + suma_negative) / iterator;
+    cout<<"\n";
+    cout<<"Suma de los positivo: "<<suma_positive;
+    cout<<"\n";
+    cout<<"Suma de los negativos: "<<suma_negative;
 }
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
